86e24ec77e2d6fb7656130d0b825d23d0931ca02
Andres Tabares Velez <atabares@visionamos.com>
2022-09-06 12:09:53 -0500

    Updates

---
+++ precache-manifest.d896f0c0f08c3b9af935acd94be88f3f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98c49d38b6c76443141a85a7292a7eb",
+    "url": "/Madeira-Hotel.Web/index.html"
+  },
+  {
+    "revision": "89c5f8a7ee60b2db434d",
+    "url": "/Madeira-Hotel.Web/static/css/2.ede86a65.chunk.css"
+  },
+  {
+    "revision": "043ccf9b29d11e781162",
+    "url": "/Madeira-Hotel.Web/static/css/main.7937548d.chunk.css"
+  },
+  {
+    "revision": "89c5f8a7ee60b2db434d",
+    "url": "/Madeira-Hotel.Web/static/js/2.b2180b67.chunk.js"
+  },
+  {
+    "revision": "ec15c8cbec563da3ac9eea5cd64d9660",
+    "url": "/Madeira-Hotel.Web/static/js/2.b2180b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043ccf9b29d11e781162",
+    "url": "/Madeira-Hotel.Web/static/js/main.57dd3e29.chunk.js"
+  },
+  {
+    "revision": "3e4c311bbf5354b365d6",
+    "url": "/Madeira-Hotel.Web/static/js/runtime-main.b7674fd8.js"
+  },
+  {
+    "revision": "c783c37bb06ece70f86418cc47b59e39",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_7890-HDR.c783c37b.jpg"
+  },
+  {
+    "revision": "8facb6cb729cfb6fff3ad29e94f2a5fb",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_7894-HDR-Editar.8facb6cb.jpg"
+  },
+  {
+    "revision": "fd4cacaa0f75d15e753d340d99cac758",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_7914-HDR-Editar.fd4cacaa.jpg"
+  },
+  {
+    "revision": "85c6147eda94985abf348c1189ee3578",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_8058-HDR-Editar.85c6147e.jpg"
+  },
+  {
+    "revision": "164a78bc0a0ad4ab7dcfbda74b047d00",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_8122-HDR-Editar.164a78bc.jpg"
+  },
+  {
+    "revision": "92392d15b69db883f26fbdebc5015b46",
+    "url": "/Madeira-Hotel.Web/static/media/DSC_8128-HDR-Editar.92392d15.jpg"
+  },
+  {
+    "revision": "c6ed81dd7b049ea2040f1300cb78060a",
+    "url": "/Madeira-Hotel.Web/static/media/about.c6ed81dd.svg"
+  },
+  {
+    "revision": "e65a6536fd277930d0d7eeccd7dfb89f",
+    "url": "/Madeira-Hotel.Web/static/media/customer.e65a6536.svg"
+  },
+  {
+    "revision": "f6f05af4ebfd528b999e303df5a90263",
+    "url": "/Madeira-Hotel.Web/static/media/customer1.f6f05af4.svg"
+  },
+  {
+    "revision": "9dac55866191ecfd805f015323ccf807",
+    "url": "/Madeira-Hotel.Web/static/media/customer3.9dac5586.svg"
+  },
+  {
+    "revision": "d1e8cb2d89d663e971662c51c0df9963",
+    "url": "/Madeira-Hotel.Web/static/media/en.d1e8cb2d.svg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/Madeira-Hotel.Web/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "c193004b3f55b9338bc1b3c9692f4744",
+    "url": "/Madeira-Hotel.Web/static/media/es.c193004b.svg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/Madeira-Hotel.Web/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/Madeira-Hotel.Web/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/Madeira-Hotel.Web/static/media/room-3.9a1103b5.jpeg"
+  }
+]);
